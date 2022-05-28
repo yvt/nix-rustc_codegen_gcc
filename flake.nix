bd135253ea77d3c3f6062b0fa4d80e1a972dd7cb
yvt <i@yvt.jp>
2022-05-28 18:59:42 +0900

    Use a custom fork for `rx-embedded-rustc_codegen_gcc`

--- flake.nix
+++ flake.nix
@@ -20,24 +20,37 @@
       url = "github:rust-lang/rustc_codegen_gcc/e6dbecdff382691b9f072fedc7cf70cd8ab5a6a4";
       flake = false;
     };
+
+    rx-embedded-rustc_codegen_gcc-src = {
+      url = "github:yvt/rustc_codegen_gcc/d753e3fb84c670994a8e12349e3a832eb4d7d3e2";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk, fenix, custom_gcc-src, rustc_codegen_gcc-src }:
+  outputs = {
+    self, nixpkgs, flake-utils, naersk, fenix, custom_gcc-src, rustc_codegen_gcc-src,
+    rx-embedded-rustc_codegen_gcc-src,
+  }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        fenixToolchain' = fenix.packages.${system}.toolchainOf { 
+        
+        rustc_codegen_gcc-toolchain = { 
           date = "2022-03-26";
           sha256 = "/GO6g+X5qEo4HIl/jTHG1S57fWil1C3/WSi/4ZwFPHU=";
         };
-        fenixToolchain = fenixToolchain'.withComponents [ "rustc" "cargo" "rustc-dev" ];
-        naersk-lib = naersk.lib."${system}".override {
-          cargo = fenixToolchain;
-          rustc = fenixToolchain;
-        };
         
-        rcgPackages = { pkgs, prefix ? "" }:
+        rcgPackages = { 
+          pkgs, prefix ? "", rustc_codegen_gcc-src, rustc_codegen_gcc-toolchain,
+        }:
           let
+            fenixToolchain = 
+              (fenix.packages.${system}.toolchainOf rustc_codegen_gcc-toolchain)
+              .withComponents [ "rustc" "cargo" "rustc-dev" ];
+            naersk-lib = naersk.lib."${system}".override {
+              cargo = fenixToolchain;
+              rustc = fenixToolchain;
+            };
             customGcc =
               (import ./modules/gcc.nix) {
                 name = "rustc_codegen_gcc";
@@ -61,9 +74,17 @@
         rec {
           packages =
             { default = packages.librustc_codegen_gcc; } //
-            (rcgPackages { inherit pkgs; }) //
+            (rcgPackages { 
+              inherit pkgs rustc_codegen_gcc-src rustc_codegen_gcc-toolchain; 
+            }) //
             (rcgPackages { 
               prefix = "rx-embedded-"; 
-              pkgs = pkgs.pkgsCross.rx-embedded.buildPackages; });
+              rustc_codegen_gcc-src = rx-embedded-rustc_codegen_gcc-src;
+              rustc_codegen_gcc-toolchain = {
+                date = "2022-03-30";
+                sha256 = "lswbVXYK4g58MZpQ6M38yismKCEjRF525FlFEZ3RWLw=";
+              };
+              pkgs = pkgs.pkgsCross.rx-embedded.buildPackages;
+            });
         });
 }
